2cbb46d1187447da06611f02a4d4ba110fbea58c
gp <g.peck.iii@gmail.com>
2023-02-19 20:06:32 -0800

    error handling and profiles

--- app/models.py
+++ app/models.py
@@ -3,7 +3,9 @@ from app import db
 from app import login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from hashlib import md5
 
+    # ...
 
 @login.user_loader
 def load_user(id):
@@ -17,6 +19,9 @@ class User(UserMixin,db.Model):
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
 
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -25,6 +30,10 @@ class User(UserMixin,db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
 
 class Post(db.Model):
